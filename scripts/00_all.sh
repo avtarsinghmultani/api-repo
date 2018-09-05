ae08233f7c9127c4679ef925499423ae91a9cad6
PMCD <Peter.McDonald@cognizant.com>
2018-09-05 12:42:03 +1000

    Updating 00_all.sh script to pause between starting services

--- scripts/00_all.sh
+++ scripts/00_all.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
+echo This will take 30 seconds
 ./01_launch_config_server.sh
+sleep 4
 ./02_launch_naming_server.sh
+sleep 4
 ./03_api_gateway_server.sh
+sleep 4
 ./04_microservice_one.sh
+sleep 4
 ./05_microservice_two.sh
+sleep 4
+echo done now try http://localhost:8761/  or   http://localhost:8765/microservice_one/one/
+
